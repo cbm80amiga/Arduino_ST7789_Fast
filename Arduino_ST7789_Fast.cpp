93fd4acf59f6d2a4084096b38e139bc3f15d64d3
cbm80amiga <pawelhernik123@gmail.com>
2020-09-18 22:50:07 +0200

    Major optimizations
    Optimized SPI fill and copy operations
    Reduced memory usage by 800-900B

--- Arduino_ST7789_Fast.cpp
+++ Arduino_ST7789_Fast.cpp
@@ -1,11 +1,60 @@
 // Fast ST7789 IPS 240x240 SPI display library
-// (c) 2019 by Pawel A. Hernik
+// (c) 2019-20 by Pawel A. Hernik
 
 #include "Arduino_ST7789_Fast.h"
 #include <limits.h>
 #include "pins_arduino.h"
 #include "wiring_private.h"
 #include <SPI.h>
+
+// -----------------------------------------
+// ST7789 commands
+
+#define ST7789_NOP     0x00
+#define ST7789_SWRESET 0x01
+
+#define ST7789_SLPIN   0x10  // sleep on
+#define ST7789_SLPOUT  0x11  // sleep off
+#define ST7789_PTLON   0x12  // partial on
+#define ST7789_NORON   0x13  // partial off
+#define ST7789_INVOFF  0x20  // invert off
+#define ST7789_INVON   0x21  // invert on
+#define ST7789_DISPOFF 0x28  // display off
+#define ST7789_DISPON  0x29  // display on
+#define ST7789_IDMOFF  0x38  // idle off
+#define ST7789_IDMON   0x39  // idle on
+
+#define ST7789_CASET   0x2A
+#define ST7789_RASET   0x2B
+#define ST7789_RAMWR   0x2C
+#define ST7789_RAMRD   0x2E
+
+#define ST7789_COLMOD  0x3A
+#define ST7789_MADCTL  0x36
+
+#define ST7789_PTLAR    0x30   // partial start/end
+#define ST7789_VSCRDEF  0x33   // SETSCROLLAREA
+#define ST7789_VSCRSADD 0x37
+
+#define ST7789_WRDISBV  0x51
+#define ST7789_WRCTRLD  0x53
+#define ST7789_WRCACE   0x55
+#define ST7789_WRCABCMB 0x5e
+
+#define ST7789_POWSAVE    0xbc
+#define ST7789_DLPOFFSAVE 0xbd
+
+// bits in MADCTL
+#define ST7789_MADCTL_MY  0x80
+#define ST7789_MADCTL_MX  0x40
+#define ST7789_MADCTL_MV  0x20
+#define ST7789_MADCTL_ML  0x10
+#define ST7789_MADCTL_RGB 0x00
+
+#define ST7789_240x240_XSTART 0
+#define ST7789_240x240_YSTART 0
+
+#define ST_CMD_DELAY   0x80
 
 // Initialization commands for ST7789 240x240 1.3" IPS
 // taken from Adafruit
@@ -33,7 +82,9 @@ static const uint8_t PROGMEM init_240x240[] = {
     ST7789_NORON  ,   ST_CMD_DELAY,  		// 8: Normal display on, no args, w/delay
       10,                     				// 10 ms delay
     ST7789_DISPON ,   ST_CMD_DELAY,  		// 9: Main screen turn on, no args, w/delay
-      20 };                  				// 255 = 500 ms delay
+      20
+};
+// -----------------------------------------
 
 #ifdef COMPATIBILITY_MODE
 static SPISettings spiSettings;
@@ -73,6 +124,7 @@ inline void Arduino_ST7789::writeSPI(uint8_t c)
     SPI.transfer(c);
 #else
     SPDR = c;
+    /*
     asm volatile("nop"); // 8 NOPs seem to be enough for 16MHz AVR @ DIV2 to avoid using while loop
     asm volatile("nop");
     asm volatile("nop");
@@ -81,10 +133,86 @@ inline void Arduino_ST7789::writeSPI(uint8_t c)
     asm volatile("nop");
     asm volatile("nop");
     asm volatile("nop");
+    */
+    asm volatile("rjmp .+0\n");
+    asm volatile("rjmp .+0\n");
+    asm volatile("rjmp .+0\n");
+    asm volatile("rjmp .+0\n");
     //while(!(SPSR & _BV(SPIF))) ;
 #endif
 }
 
+// ----------------------------------------------------------
+// fast method to send multiple 16-bit values via SPI
+inline void Arduino_ST7789::writeMulti(uint16_t color, uint16_t num)
+{
+#ifdef COMPATIBILITY_MODE
+  while(num--) { SPI.transfer(color>>8);  SPI.transfer(color); }
+#else
+  asm volatile
+  (
+  "next:\n"
+    "out %[spdr],%[hi]\n"
+    "rjmp .+0\n"  // wait 8*2+1 = 17 cycles
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "nop\n"
+    "out %[spdr],%[lo]\n"
+    "rjmp .+0\n"  // wait 6*2+1 = 13 cycles + sbiw + brne
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "nop\n"
+    "sbiw %[num],1\n"
+    "brne next\n"
+    : [num] "+w" (num)
+    : [spdr] "I" (_SFR_IO_ADDR(SPDR)), [lo] "r" ((uint8_t)color), [hi] "r" ((uint8_t)(color>>8))
+  );
+#endif
+} 
+// ----------------------------------------------------------
+// fast method to send multiple 16-bit values from RAM via SPI
+inline void Arduino_ST7789::copyMulti(uint8_t *img, uint16_t num)
+{
+#ifdef COMPATIBILITY_MODE
+  while(num--) { SPI.transfer(*(img+1)); SPI.transfer(*(img+0)); img+=2; }
+#else
+  uint8_t lo,hi;
+  asm volatile
+  (
+  "nextCopy:\n"
+    "ld  %[hi],%a[img]+\n"
+    "ld  %[lo],%a[img]+\n"
+    "out %[spdr],%[lo]\n"
+    "rjmp .+0\n"  // wait 8*2+1 = 17 cycles
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "nop\n"
+    "out %[spdr],%[hi]\n"
+    "rjmp .+0\n"  // wait 4*2+1 = 9 cycles + sbiw + brne + ld*2
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "nop\n"
+    "sbiw %[num],1\n"
+    "brne nextCopy\n"
+    : [num] "+w" (num)
+    : [spdr] "I" (_SFR_IO_ADDR(SPDR)), [img] "e" (img), [lo] "r" (lo), [hi] "r" (hi)
+  );
+#endif
+} 
 // ----------------------------------------------------------
 Arduino_ST7789::Arduino_ST7789(int8_t dc, int8_t rst, int8_t cs) : Adafruit_GFX(ST7789_TFTWIDTH, ST7789_TFTHEIGHT) 
 {
@@ -98,10 +226,17 @@ void Arduino_ST7789::init(uint16_t width, uint16_t height)
 {
   commonST7789Init(NULL);
 
-  _colstart = ST7789_240x240_XSTART;
-  _rowstart = ST7789_240x240_YSTART;
+  if(width==240 && height==240) {
+    _colstart = 0;
+    _rowstart = 80;
+  } else {
+    _colstart = 0;
+    _rowstart = 0;
+  }
+  _ystart = _xstart = 0;
   _width  = width;
   _height = height;
+
   displayInit(init_240x240);
   setRotation(2);
 }
@@ -120,13 +255,26 @@ void Arduino_ST7789::writeCmd(uint8_t c)
 }
 
 // ----------------------------------------------------------
-void Arduino_ST7789::writeData(uint8_t c) 
+void Arduino_ST7789::writeData(uint8_t d8) 
 {
   DC_DATA;
   CS_ACTIVE;
   SPI_START;
     
-  writeSPI(c);
+  writeSPI(d8);
+
+  CS_IDLE;
+  SPI_END;
+}
+
+// ----------------------------------------------------------
+void Arduino_ST7789::writeData16(uint16_t d16) 
+{
+  DC_DATA;
+  CS_ACTIVE;
+  SPI_START;
+    
+  writeMulti(d16,1);
 
   CS_IDLE;
   SPI_END;
@@ -137,17 +285,17 @@ void Arduino_ST7789::displayInit(const uint8_t *addr)
 {
   uint8_t  numCommands, numArgs;
   uint16_t ms;
-  numCommands = pgm_read_byte(addr++);   // Number of commands to follow
-  while(numCommands--) {                 // For each command...
-    writeCmd(pgm_read_byte(addr++));     //   Read, issue command
-    numArgs  = pgm_read_byte(addr++);    //   Number of args to follow
-    ms       = numArgs & ST_CMD_DELAY;   //   If hibit set, delay follows args
-    numArgs &= ~ST_CMD_DELAY;            //   Mask out delay bit
+  numCommands = pgm_read_byte(addr++);
+  while(numCommands--) {
+    writeCmd(pgm_read_byte(addr++));
+    numArgs  = pgm_read_byte(addr++);
+    ms       = numArgs & ST_CMD_DELAY;
+    numArgs &= ~ST_CMD_DELAY;
     while(numArgs--) writeData(pgm_read_byte(addr++));
 
     if(ms) {
-      ms = pgm_read_byte(addr++); // Read post-command delay time (ms)
-      if(ms == 255) ms = 500;     // If 255, delay for 500 ms
+      ms = pgm_read_byte(addr++);
+      if(ms==255) ms=500;
       delay(ms);
     }
   }
@@ -157,9 +305,6 @@ void Arduino_ST7789::displayInit(const uint8_t *addr)
 // Initialization code common to all ST7789 displays
 void Arduino_ST7789::commonST7789Init(const uint8_t *cmdList) 
 {
-  _ystart = _xstart = 0;
-  _colstart  = _rowstart = 0; // May be overridden in init func
-
   pinMode(dcPin, OUTPUT);
 #ifndef CS_ALWAYS_LOW
 	pinMode(csPin, OUTPUT);
@@ -174,12 +319,13 @@ void Arduino_ST7789::commonST7789Init(const uint8_t *cmdList)
 #endif
 #endif
 
+  // on AVR ST7789 works correctly in MODE2 and MODE3 but for STM32 only MODE3 seems to be working
   SPI.begin();
 #ifdef COMPATIBILITY_MODE
-  spiSettings = SPISettings(16000000, MSBFIRST, SPI_MODE2);  // 8000000 gives max speed on AVR 16MHz
+  spiSettings = SPISettings(16000000, MSBFIRST, SPI_MODE3);  // 8000000 gives max speed on AVR 16MHz
 #else
   SPI.setClockDivider(SPI_CLOCK_DIV2);
-  SPI.setDataMode(SPI_MODE2);
+  SPI.setDataMode(SPI_MODE3);
 #endif
 
   CS_ACTIVE;
@@ -214,62 +360,50 @@ void Arduino_ST7789::setRotation(uint8_t m)
      break;
   case 2:
      writeData(ST7789_MADCTL_RGB);
-     _xstart = _colstart;
-     _ystart = _rowstart;
+     _xstart = 0;
+     _ystart = 0;
      break;
    case 3:
      writeData(ST7789_MADCTL_MX | ST7789_MADCTL_MV | ST7789_MADCTL_RGB);
-     _ystart = _colstart;
-     _xstart = _rowstart;
+     _xstart = 0;
+     _ystart = 0;
      break;
   }
 }
 
 // ----------------------------------------------------------
-void Arduino_ST7789::setAddrWindow(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1) 
+void Arduino_ST7789::setAddrWindow(uint16_t xs, uint16_t ys, uint16_t xe, uint16_t ye)
 {
-  uint16_t xs = x0 + _xstart, xe = x1 + _xstart;
-  uint16_t ys = y0 + _ystart, ye = y1 + _ystart;
-/*
-  writeCmd(ST7789_CASET);
-  writeData(xs >> 8); writeData(xs & 0xFF);
-  writeData(xe >> 8); writeData(xe & 0xFF);
+  xs+=_xstart; xe+=_xstart;
+  ys+=_ystart; ye+=_ystart;
 
-  writeCmd(ST7789_RASET);
-  writeData(ys >> 8); writeData(ys & 0xFF);
-  writeData(ye >> 8); writeData(ye & 0xFF);
-
-  writeCmd(ST7789_RAMWR);
-*/
-
-  // optimized version
   CS_ACTIVE;
   SPI_START;
   
   DC_COMMAND; writeSPI(ST7789_CASET);
   DC_DATA;
-  writeSPI(xs >> 8); writeSPI(xs & 0xFF);
-  writeSPI(xe >> 8); writeSPI(xe & 0xFF);
+  writeSPI(xs >> 8); writeSPI(xs);
+  writeSPI(xe >> 8); writeSPI(xe);
 
   DC_COMMAND; writeSPI(ST7789_RASET);
   DC_DATA;
-  writeSPI(ys >> 8); writeSPI(ys & 0xFF);
-  writeSPI(ye >> 8); writeSPI(ye & 0xFF);
+  writeSPI(ys >> 8); writeSPI(ys);
+  writeSPI(ye >> 8); writeSPI(ye);
 
   DC_COMMAND; writeSPI(ST7789_RAMWR);
-  
-  CS_IDLE;
-  SPI_END;
+
+  DC_DATA;
+  // no CS_IDLE + SPI_END, DC_DATA to save memory
 }
 
 // ----------------------------------------------------------
 void Arduino_ST7789::pushColor(uint16_t color) 
 {
   SPI_START;
-  DC_DATA;
+  //DC_DATA;
   CS_ACTIVE;
 
-  writeSPI(color >> 8); writeSPI(color);
+  writeSPI(color>>8); writeSPI(color);
 
   CS_IDLE;
   SPI_END;
@@ -281,12 +415,30 @@ void Arduino_ST7789::drawPixel(int16_t x, int16_t y, uint16_t color)
   if(x<0 ||x>=_width || y<0 || y>=_height) return;
   setAddrWindow(x,y,x+1,y+1);
 
-  SPI_START;
-  DC_DATA;
-  CS_ACTIVE;
-
-  writeSPI(color >> 8); writeSPI(color);
-
+  writeSPI(color>>8); writeSPI(color);
+  /*
+  asm volatile
+  (
+    "out %[spdr],%[hi]\n"
+    "rjmp .+0\n"  // wait 8*2+1 = 17 cycles
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "rjmp .+0\n"
+    "nop\n"
+    "out %[spdr],%[lo]\n"
+    //"rjmp .+0\n"  // wait 6*2+1 = 13 cycles + sbiw + brne
+    //"rjmp .+0\n"
+    //"rjmp .+0\n"
+    //"rjmp .+0\n"
+    //"nop\n"
+    :
+    : [spdr] "I" (_SFR_IO_ADDR(SPDR)), [lo] "r" ((uint8_t)color), [hi] "r" ((uint8_t)(color>>8))
+  );
+  */
   CS_IDLE;
   SPI_END;
 }
@@ -298,25 +450,7 @@ void Arduino_ST7789::drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t col
   if(y+h-1>=_height) h=_height-y;
   setAddrWindow(x, y, x, y+h-1);
 
-  uint8_t hi = color >> 8, lo = color;
-    
-  SPI_START;
-  DC_DATA;
-  CS_ACTIVE;
-
-  uint8_t num8 = h>>3;
-  while(num8--) {
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-  }
-  num8 = (uint8_t)h & 7;
-  while(num8--) { writeSPI(hi); writeSPI(lo); }
+  writeMulti(color,h);
 
   CS_IDLE;
   SPI_END;
@@ -329,25 +463,7 @@ void Arduino_ST7789::drawFastHLine(int16_t x, int16_t y, int16_t w,  uint16_t co
   if(x+w-1>=_width)  w=_width-x;
   setAddrWindow(x, y, x+w-1, y);
 
-  uint8_t hi = color >> 8, lo = color;
-
-  SPI_START;
-  DC_DATA;
-  CS_ACTIVE;
-
-  uint8_t num8 = w>>3;
-  while(num8--) {
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-  }
-  num8 = (uint8_t)w & 7;
-  while(num8--) { writeSPI(hi); writeSPI(lo); }
+  writeMulti(color,w);
 
   CS_IDLE;
   SPI_END;
@@ -367,34 +483,7 @@ void Arduino_ST7789::fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16
   if(y+h-1>=_height) h=_height-y;
   setAddrWindow(x, y, x+w-1, y+h-1);
 
-  uint8_t hi = color >> 8, lo = color;
-    
-  SPI_START;
-  DC_DATA;
-  CS_ACTIVE;
-
-  uint32_t num = (uint32_t)w*h;
-  uint16_t num16 = num>>4;
-  while(num16--) {
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-    writeSPI(hi); writeSPI(lo);
-  }
-  uint8_t num8 = num & 0xf;
-  while(num8--) { writeSPI(hi); writeSPI(lo); }
+  writeMulti(color,w*h);
 
   CS_IDLE;
   SPI_END;
@@ -404,28 +493,14 @@ void Arduino_ST7789::fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16
 // draws image from RAM
 void Arduino_ST7789::drawImage(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t *img16) 
 {
-  if(x>=_width || y>=_height || w<=0 || h<=0) return;
+  // all protections should be on the application side
+  if(w<=0 || h<=0) return;  // left for compatibility
+  //if(x>=_width || y>=_height || w<=0 || h<=0) return;
+  //if(x+w-1>=_width)  w=_width -x;
+  //if(y+h-1>=_height) h=_height-y;
   setAddrWindow(x, y, x+w-1, y+h-1);
 
-  SPI_START;
-  DC_DATA;
-  CS_ACTIVE;
-
-  uint32_t num = (uint32_t)w*h;
-  uint16_t num16 = num>>3;
-  uint8_t *img = (uint8_t *)img16;
-  while(num16--) {
-    writeSPI(*(img+1)); writeSPI(*(img+0)); img+=2;
-    writeSPI(*(img+1)); writeSPI(*(img+0)); img+=2;
-    writeSPI(*(img+1)); writeSPI(*(img+0)); img+=2;
-    writeSPI(*(img+1)); writeSPI(*(img+0)); img+=2;
-    writeSPI(*(img+1)); writeSPI(*(img+0)); img+=2;
-    writeSPI(*(img+1)); writeSPI(*(img+0)); img+=2;
-    writeSPI(*(img+1)); writeSPI(*(img+0)); img+=2;
-    writeSPI(*(img+1)); writeSPI(*(img+0)); img+=2;
-  }
-  uint8_t num8 = num & 0x7;
-  while(num8--) { writeSPI(*(img+1)); writeSPI(*(img+0)); img+=2; }
+  copyMulti((uint8_t *)img16, w*h);
 
   CS_IDLE;
   SPI_END;
@@ -437,10 +512,6 @@ void Arduino_ST7789::drawImageF(int16_t x, int16_t y, int16_t w, int16_t h, cons
 {
   if(x>=_width || y>=_height || w<=0 || h<=0) return;
   setAddrWindow(x, y, x+w-1, y+h-1);
-
-  SPI_START;
-  DC_DATA;
-  CS_ACTIVE;
 
   uint32_t num = (uint32_t)w*h;
   uint16_t num16 = num>>3;
@@ -511,31 +582,25 @@ void Arduino_ST7789::resetDisplay()
 void Arduino_ST7789::setScrollArea(uint16_t tfa, uint16_t bfa) 
 {
   uint16_t vsa = 320-tfa-bfa; // ST7789 320x240 VRAM
-  writeCmd(ST7789_VSCRDEF); // SETSCROLLAREA = 0x33
-  writeData(tfa >> 8);
-  writeData(tfa);
-  writeData(vsa >> 8);
-  writeData(vsa);
-  writeData(bfa >> 8);
-  writeData(bfa);
+  writeCmd(ST7789_VSCRDEF);
+  writeData16(tfa);
+  writeData16(vsa);
+  writeData16(bfa);
 }
 
 // ----------------------------------------------------------
 void Arduino_ST7789::setScroll(uint16_t vsp) 
 {
-  writeCmd(ST7789_VSCRSADD); // VSCRSADD = 0x37
-  writeData(vsp >> 8);
-  writeData(vsp);
+  writeCmd(ST7789_VSCRSADD);
+  writeData16(vsp);
 }
 
 // ----------------------------------------------------------
 void Arduino_ST7789::setPartArea(uint16_t sr, uint16_t er) 
 {
-  writeCmd(ST7789_PTLAR);  // SETPARTAREA = 0x30
-  writeData(sr >> 8);
-  writeData(sr);
-  writeData(er >> 8);
-  writeData(er);
+  writeCmd(ST7789_PTLAR);
+  writeData16(sr);
+  writeData16(er);
 }
 
 // ----------------------------------------------------------
--- Arduino_ST7789_Fast.h
+++ Arduino_ST7789_Fast.h
@@ -1,5 +1,5 @@
 // Fast ST7789 IPS 240x240 SPI display library
-// (c) 2019 by Pawel A. Hernik
+// (c) 2019-20 by Pawel A. Hernik
 
 #ifndef _ST7789_FAST_H_
 #define _ST7789_FAST_H_
@@ -21,54 +21,11 @@
 #define ST7789_TFTWIDTH 	240
 #define ST7789_TFTHEIGHT 	240
 
-#define ST7789_240x240_XSTART 0
-#define ST7789_240x240_YSTART 0
-
-#define ST_CMD_DELAY   0x80
-
-#define ST7789_NOP     0x00
-#define ST7789_SWRESET 0x01
-
-#define ST7789_SLPIN   0x10  // sleep on
-#define ST7789_SLPOUT  0x11  // sleep off
-#define ST7789_PTLON   0x12  // partial on
-#define ST7789_NORON   0x13  // partial off
-#define ST7789_INVOFF  0x20  // invert off
-#define ST7789_INVON   0x21  // invert on
-#define ST7789_DISPOFF 0x28  // display off
-#define ST7789_DISPON  0x29  // display on
-#define ST7789_IDMOFF  0x38  // idle off
-#define ST7789_IDMON   0x39  // idle on
-
-#define ST7789_CASET   0x2A
-#define ST7789_RASET   0x2B
-#define ST7789_RAMWR   0x2C
-#define ST7789_RAMRD   0x2E
-
-#define ST7789_COLMOD  0x3A
-#define ST7789_MADCTL  0x36
-
-#define ST7789_PTLAR    0x30   // partial start/end
-#define ST7789_VSCRDEF  0x33   // SETSCROLLAREA
-#define ST7789_VSCRSADD 0x37
-
-#define ST7789_WRDISBV  0x51
-#define ST7789_WRCTRLD  0x53
-#define ST7789_WRCACE   0x55
-#define ST7789_WRCABCMB 0x5e
-
-#define ST7789_POWSAVE    0xbc
-#define ST7789_DLPOFFSAVE 0xbd
-
-// bits in MADCTL
-#define ST7789_MADCTL_MY  0x80
-#define ST7789_MADCTL_MX  0x40
-#define ST7789_MADCTL_MV  0x20
-#define ST7789_MADCTL_ML  0x10
-#define ST7789_MADCTL_RGB 0x00
-
-
 // Color definitions
+
+#define RGBto565(r,g,b) ((((r) & 0xF8) << 8) | (((g) & 0xFC) << 3) | ((b) >> 3)) 
+#define RGBIto565(r,g,b,i) ((((((r)*(i))/255) & 0xF8) << 8) | ((((g)*(i)/255) & 0xFC) << 3) | ((((b)*(i)/255) & 0xFC) >> 3)) 
+
 #define	BLACK   0x0000
 #define	BLUE    0x001F
 #define	RED     0xF800
@@ -78,7 +35,12 @@
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
 
-#define RGBto565(r,g,b) ((((r) & 0xF8) << 8) | (((g) & 0xFC) << 3) | ((b) >> 3)) 
+#define	GREY  RGBto565(128,128,128);
+#define	LGREY RGBto565(160,160,160);
+#define	DGREY RGBto565( 80, 80, 80);
+
+#define	LBLUE RGBto565(100,100,255);
+#define	DBLUE RGBto565(  0,  0,128);
 
 class Arduino_ST7789 : public Adafruit_GFX {
 
@@ -88,7 +50,7 @@ class Arduino_ST7789 : public Adafruit_GFX {
   void init(uint16_t width, uint16_t height);
   void begin() { init(ST7789_TFTWIDTH,ST7789_TFTHEIGHT); }
   void init() { init(ST7789_TFTWIDTH,ST7789_TFTHEIGHT); }
-  void setAddrWindow(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1);
+  void setAddrWindow(uint16_t xs, uint16_t ys, uint16_t xe, uint16_t ye);
   void pushColor(uint16_t color);
   void fillScreen(uint16_t color=BLACK);
   void clearScreen() { fillScreen(BLACK); }
@@ -119,12 +81,15 @@ class Arduino_ST7789 : public Adafruit_GFX {
   uint16_t rgbWheel(int idx);
 
  protected:
-  uint8_t  _colstart, _rowstart, _xstart, _ystart;
+  uint8_t _colstart, _rowstart, _xstart, _ystart;
 
   void displayInit(const uint8_t *addr);
   void writeSPI(uint8_t);
+  void writeMulti(uint16_t color, uint16_t num);
+  void copyMulti(uint8_t *img, uint16_t num);
   void writeCmd(uint8_t c);
-  void writeData(uint8_t d);
+  void writeData(uint8_t d8);
+  void writeData16(uint16_t d16);
   void commonST7789Init(const uint8_t *cmdList);
 
  private:
